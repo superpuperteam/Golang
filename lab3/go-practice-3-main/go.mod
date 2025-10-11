b5a54deda901ecbe29b0a0ceab8d434308cc881c
aminkatut <152725848+superpuperteam@users.noreply.github.com>
2025-10-11 21:20:31 +0500

    Delete lab3/go-practice-3-main directory

--- lab3/go-practice-3-main/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ssss1131/go-practice3
-
-go 1.25.1
--- lab3/go-practice-3-main/internal/db/migrations/20251007164452_create_categories_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE categories
-(
-    id      SERIAL PRIMARY KEY,
-    name    TEXT NOT NULL,
-    user_id INTEGER REFERENCES users (id),
-    CONSTRAINT categories_user_name_uniq UNIQUE (user_id, name)
-);
